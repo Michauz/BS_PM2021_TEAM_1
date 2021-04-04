c4a7c89de127525db7fe5f17840436c53d23e473
Dan <danne@ac.sce.ac.il>
2021-04-04 18:25:47 +0300

    BSPM2021T1-5, BSPM2021T1-68, BSPM2021T1-9, BSPM2021T1-72, BSPM2021T1-13

---
+++ app/src/main/java/UI/LoginActivity.java
@@ -0,0 +1,80 @@
+package UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import app.msda.qna.R;
+
+import static Adapters.Authentication.getCurrentUser;
+import static Adapters.Authentication.signIn_Email_Password;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText email;
+    private EditText password;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        if(getCurrentUser()!=null)
+            finish();
+        setListenersAndValidators();
+    }
+
+    private void setListenersAndValidators(){
+        email = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        btn = (Button)findViewById(R.id.sign_in);
+        btn.setClickable(true);
+        email.addTextChangedListener (new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2){
+                checkFields();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        password.addTextChangedListener (new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2){
+                checkFields();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    private void checkFields(){
+        if(!TextUtils.isEmpty(email.getText()) && Patterns.EMAIL_ADDRESS.matcher(email.getText()).matches()&&!TextUtils.isEmpty(password.getText()))
+            btn.setEnabled(true);
+        else
+            btn.setEnabled(false);
+    }
+
+    public void Login(View view){
+        String email = ((EditText) findViewById(R.id.username)).getText().toString();
+        String password = ((EditText) findViewById(R.id.password)).getText().toString();
+        signIn_Email_Password(this,email,password);
+        if(getCurrentUser()!=null)
+            finish();
+    }
+}
--- app/src/main/java/app/msda/qna/MainActivity.java
+++ app/src/main/java/app/msda/qna/MainActivity.java
@@ -5,6 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import UI.LoginActivity;
+import UI.SignUpActivity;
+
+import static Adapters.Authentication.getCurrentUser;
+import static Adapters.Authentication.getInstance;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +20,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Update();
     }
 
-    public void Login(View view){
+    private void Update(){
+        if(getCurrentUser()!=null){
+            ((Button)findViewById(R.id.goto_sign_in)).setVisibility(View.GONE);
+            ((Button)findViewById(R.id.goto_sign_up)).setVisibility(View.GONE);
+            ((Button)findViewById(R.id.goto_sign_out)).setVisibility(View.VISIBLE);
+            TextView user = (TextView)findViewById(R.id.user);
+            user.setVisibility(View.VISIBLE);
+            user.setText("Hey "+getCurrentUser().getEmail());
+        }
+        else{
+            ((Button)findViewById(R.id.goto_sign_in)).setVisibility(View.VISIBLE);
+            ((Button)findViewById(R.id.goto_sign_up)).setVisibility(View.VISIBLE);
+            ((Button)findViewById(R.id.goto_sign_out)).setVisibility(View.GONE);
+            ((TextView)findViewById(R.id.user)).setVisibility(View.GONE);
+        }
+    }
+    public void SignIn(View view){
         startActivity(new Intent(this, LoginActivity.class));
+    }
+    public void SignUp(View view){
+        startActivity(new Intent(this, SignUpActivity.class));
+    }
+    public void SignOut(View view){
+        getInstance().signOut();
+        Update();
     }
 }
