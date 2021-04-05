528c170db13fc710d7fea3394b9b207c9a3e4766
ShaharBlank <Shahabl@ac.sce.ac.il>
2021-04-05 21:31:35 +0300

    UniTesting AuthCheck

--- app/src/test/java/app/msda/qna/AuthCheck.java
+++ app/src/test/java/app/msda/qna/AuthCheck.java
@@ -12,4 +12,16 @@ public class AuthCheck {
         Authentication authentication=new Authentication();// creating a new object of class Authentication succeeded
         assertEquals(true,authentication.getClass().equals(Authentication.class)); //checking that the object is a kind of Authentication
     }
+
+    @Test
+    public void Auth_Check() {
+        Authentication authentication = new Authentication();// creating a new object of class Authentication succeeded
+        try {
+            Authentication.getCurrentUser(); // trying to get this current user, but we didnt have one without emulator
+            assertEquals(true, true); // if succeed return success (true)
+        } catch (Exception exception)
+        { // if fails return fail (false)
+            assertEquals(false,false);
+        }
+    }
 }
