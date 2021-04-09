0738a3363faafa84f21ad38116765b83378160f0
Shahabl <Shahabl@ac.sce.ac.il>
2021-04-10 00:58:07 +0300

    2 tasks done
    BSPM2021T1-73
    BSPM2021T1-111

--- app/src/main/java/UI/Forum/UploadPost.java
+++ app/src/main/java/UI/Forum/UploadPost.java
@@ -1,5 +1,6 @@
 package UI.Forum;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
@@ -16,7 +17,7 @@ import Adapters.Permissions;
 import app.msda.qna.R;
 
 public class UploadPost extends AppCompatActivity {
-    private static final int CAMERA_REQUEST = 1888;
+    private static final int CAMERA_REQUEST = 1888, REQUEST_CODE = 1;
     private ImageView image;
 
     @Override
@@ -29,6 +30,7 @@ public class UploadPost extends AppCompatActivity {
     }
 
     public void postQuestion(View view) {
+        
 
     }
 
@@ -36,7 +38,11 @@ public class UploadPost extends AppCompatActivity {
         if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
             Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
             startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        } else {
+            Toast.makeText(this, "Camera permission not granted.", Toast.LENGTH_LONG).show();
+            Permissions.askForCameraPerm(this);
         }
+
     }
 
     @Override
@@ -46,9 +52,21 @@ public class UploadPost extends AppCompatActivity {
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             image.setImageBitmap(photo);
-        } else {
+        } else
             Toast.makeText(this, "Camera permission not granted.", Toast.LENGTH_LONG).show();
-            Permissions.askForCameraPerm(this);
-        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE)
+            // output for user (to inform him if the permission is granted or denied)
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT);
+
     }
 }
