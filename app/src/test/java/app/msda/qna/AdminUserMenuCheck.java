a4f8985550da65f2bfc333c59b29f0cc0d27216e
Shahabl <Shahabl@ac.sce.ac.il>
2021-05-04 16:12:30 +0300

    AdminUserMenu Testing
    BSPM2021T1-116

--- app/src/test/java/app/msda/qna/AdminUserMenuCheck.java
+++ app/src/test/java/app/msda/qna/AdminUserMenuCheck.java
@@ -2,6 +2,14 @@ package app.msda.qna;
 
 import org.junit.Test;
 
-public class AdminUserMenuCheck {
+import UI.Admin.UserManagementActivity;
 
-}
+import static org.junit.Assert.assertEquals;
+
+public class AdminUserMenuCheck {
+    @Test
+    public void AdminMenu_Check() {
+        UserManagementActivity adminActivity = new UserManagementActivity();// creating a new object of class UserManagementActivity succeeded
+        assertEquals(true, adminActivity.getClass().equals(UserManagementActivity.class));
+        //checking that the object is a kind of UserManagementActivity and works well
+    }}
